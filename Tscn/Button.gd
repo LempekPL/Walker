a0b2a636726ba2200edb2708edcc38812634dba1
Wiktor-112 <71190167+Wiktor-112@users.noreply.github.com>
2020-10-14 15:59:45 +0200

    Przycisk do innej sceny

--- Tscn/Button.gd
+++ Tscn/Button.gd
@@ -5,9 +5,6 @@ extends Button
 # var a = 2
 # var b = "text"
 
-func _ready():
-	$button.connect("pushed",self,"click",[$button])
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func click(toggled, target):
-	get_tree().change_scene("WalkaScena.tscn")
+func _on_Button_pressed():
+	get_tree().change_scene("res://Tscn/WalkaScena.tscn") # Replace with function body.
+	pass
