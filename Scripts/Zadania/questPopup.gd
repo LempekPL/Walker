f0ee56e44f0eaef746d517c661c7a41593838b55
LempekPL <Superlempek2004@gmail.com>
2020-10-15 23:52:52 +0200

    popup1

---
+++ Scripts/Zadania/questPopup.gd
@@ -0,0 +1,15 @@
+extends Popup
+
+func _ready():
+	pass 
+
+func _on_Button_pressed():
+	popup_centered_ratio(0.7)
+
+
+func _on_Button2_pressed():
+	popup_centered_ratio(0.7)
+
+
+func _on_Button3_pressed():
+	popup_centered_ratio(0.7)
