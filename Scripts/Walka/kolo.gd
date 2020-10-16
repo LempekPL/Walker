da75777a9166f9c227fb55e336cfa578d4e58bc6
astropio3000 <44520613+astropio3000@users.noreply.github.com>
2020-10-16 11:55:41 +0200

    strzałka

--- Scripts/Walka/kolo.gd
+++ Scripts/Walka/kolo.gd
@@ -15,9 +15,18 @@ var file
 var jsonR
 var angles
 var prz
-var v=0.1
+var v=0.0
 var label
+
+var speed=0
+var status=0
+var licznikStatus=-1
+var zatrzymanie=0
+
 const RADIUS = 200
+
+var labels=[]
+
 func _ready():
 	file=File.new();
 	file.open("res://data/items/ekwipunek.json", file.READ)
@@ -42,6 +51,8 @@ func _ready():
 	dynamic_font.outline_color = Color( 0, 0, 0, 1 )
 	dynamic_font.use_filter = true
 	
+
+	
 	for i in jsonR.size():
 		label=Label.new()
 		label.text=jsonR[i]["name"]
@@ -52,10 +63,12 @@ func _ready():
 		label.add_font_override("font", dynamic_font)
 		label.add_color_override("font_color", Color.red)
 		add_child(label)
+		labels.insert(i, label)
 	
 	print("work")
+	print(losuj())
 	pass
-
+var li=0
 func _draw():
 	for i in prz.size():
 		var center = Vector2(0, 0)
@@ -64,12 +77,63 @@ func _draw():
 		var color = Color(jsonR[i]["color"][0], jsonR[i]["color"][1], jsonR[i]["color"][2])
 		draw_circle_arc_poly(center, RADIUS, angle_from, angle_to, color)
 		self.rotation_degrees+=v
+		controlStatus()
+		li+=1
+		if li%100==0:
+			print(li)
 	pass
+
 func _process(delta):
 	update()
 	pass
 
 func losuj():
-	var w=randi()%361
-	for i in jsonR.size():
-		
+	randomize()
+	ukryj()
+	var w=randi()%360
+	zatrzymanie=w
+	status=3
+	for i in angles.size()-1:
+		if angles[i]<w && angles[i+1]>w:
+			return jsonR[i]
+		else:
+			return jsonR[jsonR.size()-1]
+func controlStatus():
+	match status:
+		3:
+			if licznikStatus==-1:
+				speed=0.005
+				licznikStatus=300
+			elif licznikStatus>0:
+				v+=speed
+				licznikStatus-=1
+			elif licznikStatus==0:
+				status=2
+				licznikStatus=-1
+				speed=0
+		2:
+			if licznikStatus==-1:
+				speed=-0.001
+				licznikStatus=0
+			elif licznikStatus==0:
+				if v<=0.1:
+					status=1
+					licznikStatus=-1
+					speed=0
+					v=0.1
+				else:
+					v+=speed
+		1:
+			if abs(360-int(self.rotation_degrees)%361)<zatrzymanie+0.1&&abs(360-int(self.rotation_degrees)%361)>zatrzymanie-0.1:
+				v=0
+				status=0
+				pojaw()
+func ukryj():
+	for i in labels.size():
+		labels[i].visible=false
+	pass
+	
+func pojaw():
+	for i in labels.size():
+		labels[i].visible=true
+	pass
