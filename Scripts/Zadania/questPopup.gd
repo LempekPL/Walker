4726e23ed1d69d29bc4ed85364b1e76cf4cbbc4f
LempekPL <Superlempek2004@gmail.com>
2020-10-17 22:03:46 +0200

    ehhh

--- Scripts/Zadania/questPopup.gd
+++ Scripts/Zadania/questPopup.gd
@@ -15,11 +15,11 @@ var rng = RandomNumberGenerator.new()
 
 func _on_ButtonAcc_pressed():
 	file=File.new();
-	file.open("user://data/player/level.json", file.READ)
+	file.open("user://data/player/level.json", file.WRITE)
 	jsonR=JSON.parse(file.get_as_text()).result
 	rng.randomize()
 	var rand = rng.randi_range(5, 20)
-	jsonR.exp = rand
-	file.store_line(to_json(jsonR))
+	#jsonR.exp += rand
+	#file.store_line(to_json(jsonR))
 	file.close()
 	get_node("../Popup").hide()
