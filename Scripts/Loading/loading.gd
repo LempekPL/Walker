6980e443e671857ee940112c6000a5ac4d93984e
ghostovsky2 <szymonsoborowski@gmail.com>
2020-10-18 17:51:40 +0200

    better saving files

--- Scripts/Loading/loading.gd
+++ Scripts/Loading/loading.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
 var paths = ["user://enemies/", "user://items/", "user://player/", "user://quests/"]
-var files = ["user://data/enemies/data.json", "user://data/items/ekwipunek.json", "user://data/items/itemy.json", "res://data/player/data.json", "res://data/player/level.json", "res://data/player/quests.json", "res://data/quests/data.json"]
-var copyFiles = ["res://data/enemies/", "res://data/items/", "res://data/player/", "res://data/quests/"]
+var files = ["res://data/enemies/data.json", "res://data/items/ekwipunek.json", "res://data/items/itemy.json", "res://data/player/data.json", "res://data/player/level.json", "res://data/player/quests.json", "res://data/quests/data.json"]
+var copyFiles = ["user://enemies/data.json", "user://items/ekwipunek.json", "user://items/itemy.json", "user://player/data.json", "user:player/level.json", "user://player/quests.json", "user://quests/data.json"]
 
 func _ready():
 	var i = 0
 	for path in paths:
 		if(!is_dir(path)):
 			make_dr(path.split("/")[2])
-		if(!is_file(files[i])):
-			create_file(copyFiles[i], path)
+		if(!is_file(copyFiles[i])):
+			create_file(files[i], copyFiles[i])
 		i += 1
 	pass
 
