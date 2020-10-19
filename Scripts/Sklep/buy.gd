dbb6c1eb77435769d4ae91a4a90cad9b2e76f48d
ghostovsky2 <szymonsoborowski@gmail.com>
2020-10-19 18:29:20 +0200

    adding loot, uprgrading loading and fast implement random loot

--- Scripts/Sklep/buy.gd
+++ Scripts/Sklep/buy.gd
@@ -1,1 +1,17 @@
 extends Button
+
+var loot1_items
+var suma = 0
+
+func _ready():
+	openJSON()
+	for obj in loot1_items:
+		suma = suma + obj["szansa"]
+	print(suma)
+	pass
+
+func openJSON():
+	var file = File.new()
+	file.open("res://data/shop/loot1.json", File.READ)
+	loot1_items = JSON.parse(file.get_as_text()).result
+	pass
