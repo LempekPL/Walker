5e2f48e10a1a463b9e222e16811f6074ea487f8c
astropio3000 <44520613+astropio3000@users.noreply.github.com>
2020-10-25 20:47:34 +0100

    anim

--- Scripts/Walka/ButtonAkcji.gd
+++ Scripts/Walka/ButtonAkcji.gd
@@ -36,7 +36,7 @@ func _ready():
 	label=get_parent().get_node("Label")
 	koloWrog=get_parent().get_node("koloWrog/Sprite")
 	koloTy=get_parent().get_node("koloTy/Sprite")
-	tyG.set_texture(preload("res://resources/Walka/normal/pielegniarka-min.png"))
+	tyG.set_texture(load("res://resources/Walka/normal/pielegniarka-min.png"))
 	var Is = tyG.get_texture().get_size() #image size
 	var th = 5 #target height
 	var tw = 6 #target width
@@ -58,10 +58,13 @@ func _process(delta):
 
 func incjacja(_przeciwnik):
 	przeciwnik=_przeciwnik
-	var grafikaP=load(str("res://resources/Walka/normal/"+przeciwnik["name"]+".png"))	
 	get_node("/root/Node2D/Background3/koloWrog/Sprite").ustaw(_przeciwnik)
-	#wG.set_texture(grafikaP)
-	#wG.set_scale(Vector2(400, 400))
+	wG.set_texture(load("res://resources/Walka/normal/"+przeciwnik["name"]+".png"))
+	var Is = tyG.get_texture().get_size() #image size
+	var th = 5 #target height
+	var tw = 6 #target width
+	var scale = Vector2((Is.x/(Is.x/tw))/50, (Is.y/(Is.y/th))/50)
+	wG.set_scale(scale)
 	wrogHp=przeciwnik["hp"]
 	get_parent().get_node("HpWrog").text=str(wrogHp)	
 	get_parent().get_node("pasekWrog").get_node("krokibar/krokibar").max_value=int(przeciwnik["hp"])
@@ -83,40 +86,41 @@ func wylosowano():
 	label.text=item["name"]+"\n +hp: "+String(item["hp"])+" atak: "+String(item["atak"])
 	if kolejka==1:
 		tyHp-=item["atak"]
-		wrogHp+=int(item["hp"])
 		if item["atak"]>0:
 			status=false
 			tyG.set_texture(load("res://resources/Walka/damage/piel_dam-min.png"))
 			yield(get_tree().create_timer(2.0), "timeout")
 			tyG.set_texture(load("res://resources/Walka/normal/pielegniarka-min.png"))
 			status=true
+		wrogHp+=int(item["hp"])
 		if item["hp"]>0:
 			status=false
-			#wG.set_texture(load("res://resources/Walka/damage/"+przeciwnik[name]+".png"))
+			wG.set_texture(load("res://resources/Walka/healing/"+przeciwnik["name"]+".png"))
 			yield(get_tree().create_timer(2.0), "timeout")
-			#wG.set_texture(load("res://resources/Walka/normal/"+przeciwnik[name]+".png"))
+			wG.set_texture(load("res://resources/Walka/normal/"+przeciwnik["name"]+".png"))
 			status=true
 		
 	elif kolejka==0:
+		wrogHp-=item["atak"]
 		if item["atak"]>0:
 			status=false
-			#wG.set_texture(load("res://resources/Walka/damage/piel_dam-min.png"))
+			wG.set_texture(load("res://resources/Walka/damage/"+przeciwnik["name"]+".png"))
 			yield(get_tree().create_timer(2.0), "timeout")
-			#wG.set_texture(load("res://resources/Walka/normal/pielegniarka-min.png"))
+			wG.set_texture(load("res://resources/Walka/normal/"+przeciwnik["name"]+".png"))
 			status=true
-		wrogHp-=item["atak"]
+		tyHp+=item["hp"]
 		if item["hp"]>0:
 			status=false
 			tyG.set_texture(load("res://resources/Walka/healing/piel_heal-min.png"))
 			yield(get_tree().create_timer(2.0), "timeout")
 			tyG.set_texture(load("res://resources/Walka/normal/pielegniarka-min.png"))
 			status=true
-		tyHp+=item["hp"]
+
 	get_parent().get_node("HpTy").text=str(tyHp)
 	get_parent().get_node("pasekTy").get_node("krokibar")._on_kroki_updated(tyHp,0)
 	get_parent().get_node("pasekWrog").get_node("krokibar")._on_kroki_updated(wrogHp,0)
 	get_parent().get_node("HpWrog").text=str(wrogHp)
-	status=false	
+	status=false
 	if tyHp<=0:
 		przegrana()
 	elif wrogHp<=0:
